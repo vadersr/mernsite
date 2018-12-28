2f8f259e77c01e441a55603ada91d8f76a28b388
james singley <james@jamess-MacBook-Pro.local>
2018-12-28 15:34:15 -0700

    added files

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://james:Goloshes77@ds241723.mlab.com:41723/redshirtdev'
+    mongoURI: 'mongodb://james:Goloshes77@ds241723.mlab.com:41723/redshirtdev',
+    secretOrKey: 'secret'
 }
